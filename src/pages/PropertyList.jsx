06719dc9d4f00c5f03c1fe107055a75015e2e793
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2026-02-22 13:17:15 +0530

    Update PropertyList.jsx

--- src/pages/PropertyList.jsx
+++ src/pages/PropertyList.jsx
@@ -16,7 +16,7 @@ const PropertyList = () => {
   const dispatch = useDispatch()
   const getPropertyList = async () => {
     try {
-      const response = await fetch(`https://mernfront-6na1.onrender.com/users/${user._id}/properties`, {
+      const response = await fetch(`https://mernback-1-05h8.onrender.com/users/${user._id}/properties`, {
         method: "GET"
       })
       const data = await response.json()
