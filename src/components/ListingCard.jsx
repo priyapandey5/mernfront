edb379da0182264f1e1c999f68d1ec95c7c4fb5c
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2026-02-22 13:13:14 +0530

    Update ListingCard.jsx

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -50,7 +50,7 @@ const ListingCard = ({
   const patchWishList = async () => {
     if (user?._id !== creator._id) {
     const response = await fetch(
-      `https://mernfront-6na1.onrender.com/users/${user?._id}/${listingId}`,
+      `https://mernback-1-05h8.onrender.com/users/${user?._id}/${listingId}`,
       {
         method: "PATCH",
         header: {
