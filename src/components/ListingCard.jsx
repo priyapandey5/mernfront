6815d3585e7a0305071fc971ba824cfc24d7bede
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2026-02-22 11:48:38 +0530

    Update ListingCard.jsx

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -50,7 +50,7 @@ const ListingCard = ({
   const patchWishList = async () => {
     if (user?._id !== creator._id) {
     const response = await fetch(
-      `https://mernback-kifu.onrender.com/users/${user?._id}/${listingId}`,
+      `https://mernfront-6na1.onrender.com/users/${user?._id}/${listingId}`,
       {
         method: "PATCH",
         header: {
@@ -78,7 +78,7 @@ const ListingCard = ({
           {listingPhotoPaths?.map((photo, index) => (
             <div key={index} className="slide">
               <img
-                src={`https://mernback-kifu.onrender.com/${photo?.replace("public", "")}`}
+                src={`https://mernfront-6na1.onrender.com/${photo?.replace("public", "")}`}
                 alt={`photo ${index + 1}`}
               />
               <div
